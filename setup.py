6350529e9a06a05ae99b55d7c26a653c3a6504a6
aplumin <96194310+aplumin@users.noreply.github.com>
2026-04-06 11:15:21 +0200

    Add requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from setuptools import find_packages, setup
 
 setup(
+    name="master-thesis",
+    author="Aaron Plumin",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    install_requires=[
+        "diffrax==0.7.2",
+        "jax=0.7.2",
+        "jupyter=1.1.1",
+        "numba==0.64.0",
+        "seaborn=0.13.2",
+        "snakemake==9.19.0",
+    ],
+    python_requires=">=3.11",
 )
