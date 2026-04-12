e501ce0a08a5077a27b6a9ea0f4cbb645f56e3ad
aplumin <96194310+aplumin@users.noreply.github.com>
2026-04-12 18:19:46 +0200

    Stochastic model: flexible delay chain length and exact integration of delay chain between events

--- src/models/gillespie.py
+++ src/models/gillespie.py
@@ -1,4 +1,6 @@
 """
+TODO: add superspreading (draw from negative binomial)
+
 Stochastic compartmental models:
     - gillespie_SEIPAR_W with presymptomatic and asymptomatic transmission and wastewater feedback
 """
@@ -8,27 +10,38 @@ from numba import njit
 
 @njit
 def gillespie_SEIPAR_W(params, N: int, t1: float):
-    """Gillespie algorithm combined with continuous deterministic ODEs for the delay compartments."""
+    """Gillespie algorithm with exact integration of the delay compartments between events."""
     max_events = int(N * 10)
     times = np.zeros(max_events)
     t = 0.0
 
-    states = np.zeros((max_events, 9), dtype=np.float64) 
-    current_state = np.array([float(N-1), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], dtype=np.float64)
+    # initialise states
+    num_delay_compartments = params.num_delay_compartments
+    num_states = 6 + num_delay_compartments
+    states = np.zeros((max_events, num_states), dtype=np.float64)
+    current_state = np.zeros(num_states, dtype=np.float64)
+    current_state[0] = float(N - 1) # S
+    current_state[1] = 1.0 # E
     states[0] = current_state
 
     r_const = params.p * params.phi * params.mu_a_inv + (1.0 - params.p) * (params.sigma_inv + params.mu_s_inv)
-    delay_rate = 3.0 / params.tau
-    
+    delay_rate = float(num_delay_compartments) / params.tau
     a = np.zeros(6, dtype=np.float64)
+    coeffs = np.zeros(num_delay_compartments, dtype=np.float64)
     step = 1
 
     while t < t1 and step < max_events:
-        S, E, Ia, Ip, Is, R, W1, W2, W3 = current_state
-        f_W3 = 1.0 - (params.epsilon_w / (1.0 + np.exp(-params.k * (W3 - params.R_crit))))
+        S  = current_state[0]
+        E  = current_state[1]
+        Ia = current_state[2]
+        Ip = current_state[3]
+        Is = current_state[4]
+        W_out = current_state[5 + num_delay_compartments]
+
+        f_W = 1.0 - (params.epsilon_w / (1.0 + np.exp(-params.k * (W_out - params.R_crit))))
 
         # propensities
-        a[0] = f_W3 * params.beta * (params.phi*Ia + Ip + (1.0-params.epsilon_s)*Is) * (S/N)
+        a[0] = f_W * params.beta * (params.phi*Ia + Ip + (1.0-params.epsilon_s)*Is) * (S/N)
         a[1] = params.p * E / params.gamma_inv
         a[2] = (1.0 - params.p) * E / params.gamma_inv
         a[3] = Ip / params.sigma_inv
@@ -41,13 +54,24 @@ def gillespie_SEIPAR_W(params, N: int, t1: float):
 
         # draw time step
         r1 = np.random.random()
-        time_step = (1.0 / a0) * -np.log(r1) 
-        
+        time_step = -np.log(r1) / a0
+
         # delay compartments W
-        Rt = (params.beta * r_const) * params.rho * f_W3 * (S/N)
-        current_state[6] += delay_rate * (Rt - W1) * time_step
-        current_state[7] += delay_rate * (W1 - W2) * time_step
-        current_state[8] += delay_rate * (W2 - W3) * time_step
+        Rt = (params.beta * r_const) * params.rho * f_W * (S/N)
+        xi_dt = delay_rate * time_step
+        exp_neg_xi_dt = np.exp(-xi_dt)
+
+        # W_i = Rt + exp(-xi*dt) Sum_{j≤i} (xi*dt)^(i-j) / (i-j)! * (W_j - Rt)
+        # with poisson coeffs (xi*dt)^k / k!
+        coeffs[0] = 1.0
+        for k in range(1, num_delay_compartments):
+            coeffs[k] = coeffs[k-1] * xi_dt / k
+
+        for i in range(num_delay_compartments):
+            W_i = Rt
+            for j in range(i+1):
+                W_i += exp_neg_xi_dt * coeffs[i-j] * (current_state[6+j] - Rt)
+            current_state[6+i] = W_i
         
         # draw next event
         r2 = np.random.random() * a0
