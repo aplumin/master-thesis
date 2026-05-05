49d72eede95271398d3a017df05552d014f43abf
aplumin <96194310+aplumin@users.noreply.github.com>
2026-05-05 09:50:37 +0200

    Fix delayed intervention plot: show B delay effect

--- src/models/scenarios.py
+++ src/models/scenarios.py
@@ -9,7 +9,7 @@ from typing import Callable
 from models.parameters import Params, logistic_response_function
 
 
-# TODO: should change from predefined time to stable number after interventions and before natural depletion of susceptibles
+# TODO: also change to oscillation-aligned average here
 @partial(jax.jit, static_argnames=['model', 't1'])
 def compute_R_grid(model: Callable, base_params: Params, eps_ww: float, eps_ss: float, t1: float = 100.0, E0: float = 1e-6):
     """Compute a 2D grid of Rt values with wastewater warning response efficacy on the x axis and isolation efficacy on the y axis."""
@@ -62,12 +62,12 @@ def compute_asymptomatic_grid_Itot(model: Callable, base_params: Params, p: floa
 def compute_I_tot_grid_delayed_ww(model: Callable, base_params: Params, taus, I_crit_list, t1: float = 100.0, E0: float = 1e-6):
     """
     Compute a 2D grid of proportion infected relative to baseline across different
-    wastewater reporting delays and infection intervention thresholds.
+    behavioural delays and infection intervention thresholds.
     """
     def I_tot(tau_B, I_crit):
-        _, yy = model(params=base_params.update(tau_W=tau_B, I_crit=I_crit), t1=t1, E0=E0)
+        _, yy = model(params=base_params.update(tau_B=tau_B, I_crit=I_crit), t1=t1, E0=E0)
         return yy[0,0] - yy[-1,0]
     
     I_tot_grid = jax.vmap(jax.vmap(I_tot, in_axes=(0, None)), in_axes=(None, 0))(taus, I_crit_list)
-    _, yy_base = model(params=base_params.update(I_crit=0.0), t1=t1, E0=E0)
+    _, yy_base = model(params=base_params.update(epsilon_w=0.0), t1=t1, E0=E0)
     return I_tot_grid / (yy_base[0,0] - yy_base[-1,0])
